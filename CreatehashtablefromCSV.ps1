6bfd9ce5fc22ece85211a1086f7c6d59b897eaf1
sam-solo-works <57299464+sam-solo-works@users.noreply.github.com>
2022-03-11 16:38:36 -0500

    Add files via upload

---
+++ CreatehashtablefromCSV.ps1
@@ -0,0 +1,20 @@
+﻿# create a scriptblock using the string
+$scriptBlock = [scriptblock]::Create('@{"R"="red";"B"="Blue"')
+# execute it to create the hashtable
+$tags = (& $scriptBlock)
+
+$vmtags = '@{"R"="red";"B"="Blue"}', '@{"G"="green";"A"="Alpha"}'
+
+# first loop creates the hashtables from the individual strings
+$arr = $vmtags | ForEach-Object {
+    $scriptBlock = [scriptblock]::Create($_)
+    & $scriptBlock
+}
+
+# the second loop merges all Hashtables in the array into one
+$tags = @{}
+$arr | ForEach-Object {
+    foreach ($key in $_.Keys) {
+        $tags[$key] = $_.$key
+    }
+}
