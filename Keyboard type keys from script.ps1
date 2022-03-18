10d21777a2b171f0a58cf1a2dadf12aa781213a6
sam-solo-works <57299464+sam-solo-works@users.noreply.github.com>
2022-03-18 10:19:06 -0400

    Add files via upload
    This script is kind of wonky so you have to play with it some but you are able to get powershell to type anything including special characters, spaces, and return keys for you if you use what I have here. If you apply it to a hot key then it will send whatever message you want everytime although you might have to add a portion to make sure you have the correct window selected prior to typing otherwise that's up to the user.

---
+++ Keyboard type keys from script.ps1
@@ -0,0 +1,22 @@
+﻿#$asterisk = [char]U+002A
+#$ampersand = [char]U+0026
+$P = '{*}{&}t35T{&}{*}'
+
+
+function Do-SendKeys {
+    param (
+        $SENDKEYS,
+        $WINDOWTITLE
+    )
+    #$wshell.AppActivate('title of the application window')
+    $wshell = New-Object -ComObject wscript.shell;
+    IF ($WINDOWTITLE) {$wshell.AppActivate($WINDOWTITLE)}
+    Sleep 1
+    IF ($SENDKEYS) {$wshell.SendKeys($SENDKEYS)}
+}
+Sleep 2
+Do-SendKeys -WINDOWTITLE Print -SENDKEYS $P
+
+#Do-SendKeys -WINDOWTITLE Print -SENDKEYS '{TAB}{TAB}'
+#Do-SendKeys -WINDOWTITLE Print
+#Do-SendKeys -SENDKEYS '%{f4}'
